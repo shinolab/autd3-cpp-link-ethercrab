f9de7ac47461f574d527f55670fae4ef218272be
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2025-11-19 18:11:13 +0900

    use ninja for test

--- tests/gtest.cmake
+++ tests/gtest.cmake
@@ -1,10 +1,10 @@
 include(FetchContent)
 FetchContent_Declare(
   googletest
-  URL https://github.com/google/googletest/archive/refs/tags/v1.14.0.zip
+  URL https://github.com/google/googletest/archive/refs/tags/v1.17.0.zip
 )
-set(BUILD_GMOCK OFF)
-set(INSTALL_GTEST OFF)
+set(BUILD_GMOCK OFF CACHE BOOL "" FORCE)
+set(INSTALL_GTEST OFF CACHE BOOL "" FORCE)
 if(WIN32)
   set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 endif()
